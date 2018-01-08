59ce4c1687d019da4f226777d7b9d0449f02c02e
ural <sevomax@gmail.com>
2018-01-08 15:36:12 -0500

    finaly figured out the problem with newer version of React. needed ReactDOM.render instead of React.render

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-/*$ = jQuery = require('jquery');
+$ = jQuery = require('jquery');
 var React = require('react');
-var Home = require('./components/homePage');*/
+var ReactDOM = require('react-dom');
+var Home = require('./components/homePage');
 
+/*
 $ = jQuery = require('jquery');
 import React from 'react';
 import ReactDOM from 'react-dom';
+*/
 
 
 
 
-React.render(<Home />  , document.getElementById('app'));
+ReactDOM.render(<Home />  , document.getElementById('app'));
